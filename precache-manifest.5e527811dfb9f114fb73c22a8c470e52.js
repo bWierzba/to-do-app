2d126b3f2fc5c9e663bdc2926895a38d3a86740c
wierzba <wierzbabartlomiej@gmail.com>
2020-11-24 12:19:38 +0100

    Updates

---
+++ precache-manifest.5e527811dfb9f114fb73c22a8c470e52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd29c709acd78dd0fecbd056e3a22868",
+    "url": "/to-do-app/index.html"
+  },
+  {
+    "revision": "3964ddc324ac9cb1671b",
+    "url": "/to-do-app/static/css/main.c2c949c1.chunk.css"
+  },
+  {
+    "revision": "126cc3652e67fd2bbed2",
+    "url": "/to-do-app/static/js/2.bc3d110c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-app/static/js/2.bc3d110c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3964ddc324ac9cb1671b",
+    "url": "/to-do-app/static/js/main.833c45ee.chunk.js"
+  },
+  {
+    "revision": "7bc324183290327832b6",
+    "url": "/to-do-app/static/js/runtime-main.5b7ff43f.js"
+  }
+]);
